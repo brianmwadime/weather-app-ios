322a3bccde4ba1094bb28c1a82efbe0a1e362de4
Brian Mwadime <brian.mwadime@gmail.com>
2023-01-05 18:08:19 +0300

    CoreData offline
    - setup offline mode

---
+++ Weather/Models/FavoriteLocation+CoreDataProperties.swift
@@ -0,0 +1,26 @@
+//
+//  FavoriteLocation+CoreDataProperties.swift
+//  Weather
+//
+//  Created by Brian Mwakima on 1/5/23.
+//
+//
+
+import Foundation
+import CoreData
+
+extension FavoriteLocation {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<FavoriteLocation> {
+        return NSFetchRequest<FavoriteLocation>(entityName: "FavoriteLocation")
+    }
+
+    @NSManaged public var city: String?
+    @NSManaged public var latitude: Double
+    @NSManaged public var longitude: Double
+
+}
+
+extension FavoriteLocation: Identifiable {
+
+}
