fbd57d452db98e6bcdfe04dc185081caea8f2aa5
Brian Mwadime <brian.mwadime@gmail.com>
2023-01-11 00:19:37 +0300

    Add Environment prop for setting the app background color

--- Weather/Views/ContentView.swift
+++ Weather/Views/ContentView.swift
@@ -9,12 +9,23 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
-  @StateObject var currentViewModel: CurrentViewModel
-  @StateObject var forecastViewModel: ForecastViewModel
-  @StateObject var favoritesViewModel: FavoritesViewModel
+  @ObservedObject var currentViewModel: CurrentViewModel
+  @ObservedObject var forecastViewModel: ForecastViewModel
+  @ObservedObject var favoritesViewModel: FavoritesViewModel
   @State var isFavoriteLocations: Bool = false
   @EnvironmentObject var locationService: LocationService
   @Environment(\.openURL) var openURL
+  @Environment(\.appBackgroundColor) var appBackgroundColor
+
+  init(currentViewModel: CurrentViewModel,
+       forecastViewModel: ForecastViewModel,
+       favoritesViewModel: FavoritesViewModel) {
+    self.currentViewModel = currentViewModel
+    self.favoritesViewModel = favoritesViewModel
+    self.forecastViewModel = forecastViewModel
+
+    applyNavigationStyling()
+  }
 
   var body: some View {
       NavigationView {
@@ -22,6 +33,7 @@ struct ContentView: View {
           if locationService.status == .available {
             Color(currentViewModel.condition)
                 .ignoresSafeArea(.all)
+                .setAppBackgroundColor(Color(currentViewModel.condition))
               ScrollView {
                 VStack {
                   CurrentView(vm: currentViewModel)
@@ -77,5 +89,40 @@ struct ContentView: View {
       destination: MapView(viewModel: favoritesViewModel)) {
         Image(systemName: "map")
       }
+  }
+
+  func applyNavigationStyling() {
+    // this is not the same as manipulating the proxy directly
+    let appearance = UINavigationBarAppearance()
+
+    // this overrides everything you have set up earlier.
+    appearance.configureWithTransparentBackground()
+
+    // this only applies to big titles
+    appearance.largeTitleTextAttributes = [
+      NSAttributedString.Key.foregroundColor: UIColor.white
+    ]
+    // this only applies to small titles
+    appearance.titleTextAttributes = [
+      NSAttributedString.Key.foregroundColor: UIColor.white
+    ]
+
+//    appearance.backgroundColor = UIColor(appBackgroundColor)
+    appearance.shadowImage = UIImage()
+    appearance.shadowColor = .clear
+
+    // In the following two lines you make sure that you apply the style for good
+    UINavigationBar.appearance().scrollEdgeAppearance = appearance
+    UINavigationBar.appearance().standardAppearance = appearance
+    UINavigationBar.appearance().compactScrollEdgeAppearance = appearance
+
+    // This property is not present on the UINavigationBarAppearance
+    // object for some reason and you have to leave it til the end
+    UINavigationBar.appearance().tintColor = .white
+    UINavigationBar.appearance().barTintColor = .white
+
+    UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).backgroundColor = .white
+    UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).tintColor = .black
+    UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).setTitleTextAttributes([.foregroundColor: UIColor.white], for: .normal)
   }
 }
