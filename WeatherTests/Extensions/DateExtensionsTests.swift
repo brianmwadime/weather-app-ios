4f1a5e9c3b854b46991421e057b8f7655c51dc9e
Brian Mwadime <brian.mwadime@gmail.com>
2023-01-11 15:20:52 +0300

    Add tests for offset

--- WeatherTests/Extensions/DateExtensionsTests.swift
+++ WeatherTests/Extensions/DateExtensionsTests.swift
@@ -17,10 +17,17 @@ final class DateExtensionsTests: XCTestCase {
     XCTAssertEqual(date.dayOfTheWeek, "Wednesday")
   }
 
-  func test_DAte_format_hh_mm_a() {
+  func test_Date_format_hh_mm_a() {
     let date = Date(timeIntervalSince1970: 1553709600)
     let formattedDate = date.format()
     print(formattedDate)
     XCTAssertEqual(formattedDate, "09:00 PM")
+  }
+
+  func test_Date_format_with_offset_hh_mm_a() {
+    let date = Date(timeIntervalSince1970: 1553709600)
+    let formattedDate = date.format(with: 3600)
+    print(formattedDate)
+    XCTAssertEqual(formattedDate, "10:00 PM")
   }
 }
