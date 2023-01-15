e7ca320a5073645bbcd0fad0eda69ae938f41bd5
Brian Mwadime <brian.mwadime@gmail.com>
2023-01-16 01:39:19 +0300

    Refactor Tests

--- WeatherTests/Models/MapAnnotationTests.swift
+++ WeatherTests/Models/MapAnnotationTests.swift
@@ -13,11 +13,18 @@ final class MapAnnotationTests: XCTestCase {
 
   func test_MapAnnotation_has_name_coordinates() {
 
-    let sut = MapAnnotation(name: "name", coordinate: CLLocationCoordinate2D(latitude: 0.01, longitude: 31.2))
+    let sut = MapAnnotation(id: UUID(), name: "name", coordinate: CLLocationCoordinate2D(latitude: 0.01, longitude: 31.2))
 
     XCTAssertNotNil(sut)
     XCTAssertNotNil(sut.name)
     XCTAssertNotNil(sut.coordinate)
+  }
+
+  func test_MapAnnotation_is_my_location() {
+
+    let sut = MapAnnotation(id: UUID(), name: "My Location", coordinate: CLLocationCoordinate2D(latitude: 0.01, longitude: 31.2))
+
+    XCTAssertTrue(sut.isMyLocation)
   }
 
 }
